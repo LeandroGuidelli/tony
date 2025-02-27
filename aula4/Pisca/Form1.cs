f368ba89956a4150fa438359d9594bd5731c3525
Leandro Soares Guidelli <102395460+LeandroGuidelli@users.noreply.github.com>
2025-02-26 21:44:57 -0300

    Add files via upload

---
+++ aula4/Pisca/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pisca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Image = Image.FromFile("C:\\Imagens\\Desligado.bmp");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.Tag = "Desligado";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pictureBox1.Tag.ToString() == "Desligado") {
+                pictureBox1.Visible = false;
+                pictureBox2.Visible = true;
+                pictureBox2.Image = Image.FromFile("C:\\Imagens\\Ligado.bmp");
+                pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Tag = "Ligado";
+            }
+            else if (pictureBox1.Tag.ToString() == "Ligado") {
+                pictureBox2.Visible = false;
+                pictureBox1.Visible= true;
+                pictureBox1.Image = Image.FromFile("C:\\Imagens\\Desligado.bmp");
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Tag = "Desligado";
+            }
+
+        }
+    }
+}
