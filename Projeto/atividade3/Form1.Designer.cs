576b7f530e2d6abbbed743e134a9698a31e990c3
Leandro Soares Guidelli <102395460+LeandroGuidelli@users.noreply.github.com>
2025-03-05 22:01:10 -0300

    Add files via upload

---
+++ Projeto/atividade3/Form1.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace atividade3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.TimerRua = new System.Windows.Forms.Timer(this.components);
+            this.TimerAvenida = new System.Windows.Forms.Timer(this.components);
+            this.TimerEmergencia = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.Location = new System.Drawing.Point(15, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(15, 125);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(15, 231);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(17, 19);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(17, 125);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Location = new System.Drawing.Point(17, 231);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox6.TabIndex = 7;
+            this.pictureBox6.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(21, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 52);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Liga";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(127, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 52);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Desliga";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(70, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 52);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 432);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(720, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Click += new System.EventHandler(this.toolStripStatusLabel1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(487, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 41);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Tempo";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(70, 160);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 77);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "EMERGENCIA";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(70, 252);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 24);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Polícia";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Silver;
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(437, 98);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(243, 297);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Controlador";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Black;
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.pictureBox2);
+            this.groupBox2.Controls.Add(this.pictureBox3);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(61, 60);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(134, 347);
+            this.groupBox2.TabIndex = 14;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Rua";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.Black;
+            this.groupBox3.Controls.Add(this.pictureBox4);
+            this.groupBox3.Controls.Add(this.pictureBox5);
+            this.groupBox3.Controls.Add(this.pictureBox6);
+            this.groupBox3.ForeColor = System.Drawing.Color.White;
+            this.groupBox3.Location = new System.Drawing.Point(233, 60);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(134, 347);
+            this.groupBox3.TabIndex = 15;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Avenida";
+            // 
+            // TimerRua
+            // 
+            this.TimerRua.Enabled = true;
+            this.TimerRua.Interval = 1000;
+            this.TimerRua.Tick += new System.EventHandler(this.TimerRua_Tick);
+            // 
+            // TimerAvenida
+            // 
+            this.TimerAvenida.Enabled = true;
+            this.TimerAvenida.Interval = 1000;
+            this.TimerAvenida.Tick += new System.EventHandler(this.TimerAvenida_Tick);
+            // 
+            // TimerEmergencia
+            // 
+            this.TimerEmergencia.Enabled = true;
+            this.TimerEmergencia.Interval = 1000;
+            this.TimerEmergencia.Tick += new System.EventHandler(this.TimerEmergencia_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 454);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Atividade-3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Timer TimerRua;
+        private System.Windows.Forms.Timer TimerAvenida;
+        private System.Windows.Forms.Timer TimerEmergencia;
+    }
+}
+
---
+++ Projeto/atividade3/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace atividade3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarImagens();
+            TimerRua.Stop();
+            TimerAvenida.Stop();
+            TimerEmergencia.Stop();
+        }
+
+        private void InicializarImagens()
+        {
+            // Definir todas as imagens como "Apagado" no início
+            PictureBox[] semaforos = { pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6 };
+            foreach (var semaforo in semaforos)
+            {
+                semaforo.Image = Image.FromFile("C:\\Imagens\\Apagado.bmp");
+                semaforo.SizeMode = PictureBoxSizeMode.StretchImage;
+                semaforo.Tag = "Apagado";
+            }
+        }
+
+        private void InicializarImagensEmergencia()
+        {
+            // Definir todas as imagens como "Apagado" no início
+            PictureBox[] semaforos = { pictureBox1, pictureBox2, pictureBox3, pictureBox4, pictureBox5, pictureBox6 };
+            foreach (var semaforo in semaforos)
+            {
+                semaforo.Image = Image.FromFile("C:\\Imagens\\Amarelo.bmp");
+                semaforo.SizeMode = PictureBoxSizeMode.StretchImage;
+                semaforo.Tag = "Amarelo";
+            }
+        }
+
+        private bool emergenciaAtiva = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("C:\\Imagens\\Vermelho.bmp");
+            pictureBox1.Tag = "Vermelho";
+
+            pictureBox6.Image = Image.FromFile("C:\\Imagens\\Verde.bmp");
+            pictureBox6.Tag = "Verde";
+
+            TimerRua.Interval = 3000;
+            TimerAvenida.Interval = 2000;
+            TimerRua.Start();
+            TimerAvenida.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InicializarImagens();
+            TimerRua.Stop();
+            TimerAvenida.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+            toolStripStatusLabel1.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label3.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void TimerRua_Tick(object sender, EventArgs e)
+        {
+            if (!TimerRua.Enabled) return;
+
+            if (pictureBox1.Tag.ToString() == "Vermelho")
+            {
+                TimerRua.Stop();
+                pictureBox1.Image = Image.FromFile("C:\\Imagens\\Apagado.bmp");
+                pictureBox1.Tag = "Apagado";
+
+                pictureBox3.Image = Image.FromFile("C:\\Imagens\\Verde.bmp");
+                pictureBox3.Tag = "Verde";
+
+                TimerRua.Interval = 2000; // verde para o amarelo
+                TimerRua.Start();
+            }
+            else if (pictureBox3.Tag.ToString() == "Verde")
+            {
+                TimerRua.Stop();
+                pictureBox3.Image = Image.FromFile("C:\\Imagens\\Apagado.bmp");
+                pictureBox3.Tag = "Apagado";
+
+                pictureBox2.Image = Image.FromFile("C:\\Imagens\\Amarelo.bmp");
+                pictureBox2.Tag = "Amarelo";
+
+                TimerRua.Interval = 1000; // amarelo para o vermelho
+                TimerRua.Start();
+            }
+            else if (pictureBox2.Tag.ToString() == "Amarelo")
+            {
+                TimerRua.Stop();
+                pictureBox2.Image = Image.FromFile("C:\\Imagens\\Apagado.bmp");
+                pictureBox2.Tag = "Apagado";
+
+                pictureBox1.Image = Image.FromFile("C:\\Imagens\\Vermelho.bmp");
+                pictureBox1.Tag = "Vermelho";
+
+                TimerRua.Interval = 3000; // vermelho para o verde
+                TimerRua.Start();
+            }
+        }
+
+        private void TimerAvenida_Tick(object sender, EventArgs e)
+        {
+            if (!TimerAvenida.Enabled) return;
+
+            if (pictureBox6.Tag.ToString() == "Verde")
+            {
+                TimerAvenida.Stop();
+                pictureBox6.Image = Image.FromFile("C:\\Imagens\\Apagado.bmp");
+                pictureBox6.Tag = "Apagado";
+
+                pictureBox5.Image = Image.FromFile("C:\\Imagens\\Amarelo.bmp");
+                pictureBox5.Tag = "Amarelo";
+
+                TimerAvenida.Interval = 1000; // amarelo para o vermelho
+                TimerAvenida.Start();
+            }
+            else if (pictureBox5.Tag.ToString() == "Amarelo")
+            {
+                TimerAvenida.Stop();
+                pictureBox5.Image = Image.FromFile("C:\\Imagens\\Apagado.bmp");
+                pictureBox5.Tag = "Apagado";
+
+                pictureBox4.Image = Image.FromFile("C:\\Imagens\\Vermelho.bmp");
+                pictureBox4.Tag = "Vermelho";
+
+                TimerAvenida.Interval = 3000; // vermelho para o verde
+                TimerAvenida.Start();
+            }
+            else if (pictureBox4.Tag.ToString() == "Vermelho")
+            {
+                TimerAvenida.Stop();
+                pictureBox4.Image = Image.FromFile("C:\\Imagens\\Apagado.bmp");
+                pictureBox4.Tag = "Apagado";
+
+                pictureBox6.Image = Image.FromFile("C:\\Imagens\\Verde.bmp");
+                pictureBox6.Tag = "Verde";
+
+                TimerAvenida.Interval = 2000; // verde para o amarelo
+                TimerAvenida.Start();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (!emergenciaAtiva)
+            {
+                emergenciaAtiva = true;
+                TimerRua.Stop();
+                TimerAvenida.Stop();
+                TimerEmergencia.Start();
+            }
+            else
+            {
+                emergenciaAtiva = false;
+                TimerEmergencia.Stop();
+                InicializarImagens(); // Voltar ao normal
+                TimerRua.Start();
+                TimerAvenida.Start();
+            }
+        }
+
+
+        private void TimerEmergencia_Tick(object sender, EventArgs e)
+        {
+            TimerRua.Stop();
+            TimerAvenida.Stop();
+            InicializarImagensEmergencia();
+        }
+    }
+}
